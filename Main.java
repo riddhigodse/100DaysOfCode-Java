a20dbefca6b7da2d632355d5c0774e9fb8b0865c
riddhigodse <godsev771@gmail.com>
2025-06-30 08:59:59 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Day1Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World! ");
+        System.out.println("I have started my #100DaysOfCode journey with Java! ");
+        System.out.println("Let's do this! ");
+    }
+}
